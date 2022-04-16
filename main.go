0917df74917cc324d62b384d54909d7ffdb6c11f
Derek McKinnon <mckinnon.derek@gmail.com>
2022-04-15 20:24:34 -0400

    Add basic login page

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
-func main() {
-	fmt.Println("Hello, world!")
+type LoginForm struct {
+	Username string `form:"username"`
+	Password string `form:"password"`
 }
 
-func Foo() int {
-	return 1
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/login", func(c *gin.Context) {
+		c.HTML(200, "login.tmpl", gin.H{})
+	})
+
+	r.POST("/login", func(c *gin.Context) {
+		var form LoginForm
+		c.Bind(&form)
+		c.HTML(200, "login.tmpl", gin.H{
+			"Username": form.Username,
+			"Error":    "Unknown username or password",
+		})
+	})
+
+	err := r.Run()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
