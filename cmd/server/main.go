888ca14f66768166ba0c94ca9890605a8602c55f
Derek McKinnon <mckinnon.derek@gmail.com>
2023-10-19 15:58:55 -0400

    Generate development cert and key on IdP boot

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"crypto"
-	"crypto/x509"
-	"encoding/pem"
 	idp "github.com/derekmckinnon/test-saml-idp"
 	"github.com/spf13/viper"
 	"log"
-	"os"
 )
 
 func main() {
@@ -18,21 +14,17 @@ func main() {
 		log.Fatalf("error loading configuration: %v", err)
 	}
 
-	key, err := loadPrivateKey()
+	log.Println("Generating development certificate")
+	cert, key, err := idp.GenerateDevelopmentCertificate()
 	if err != nil {
-		log.Fatalf("cannot load private key: %v", err)
-	}
-
-	certificate, err := loadCertificate()
-	if err != nil {
-		log.Fatalf("cannot load certificate: %v", err)
+		log.Fatalf("could not generate development certificate: %v", err)
 	}
 
 	log.Println("Initializing IdP Server...")
 	server := idp.New(idp.ServerOptions{
 		Config:      config,
 		Key:         key,
-		Certificate: certificate,
+		Certificate: cert,
 	})
 
 	err = server.LoadUsers(config.Users)
@@ -77,33 +69,4 @@ func loadConfig() (*idp.Config, error) {
 	}
 
 	return config, nil
-}
-
-func loadPemFile(path string) (*pem.Block, error) {
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	block, _ := pem.Decode(data)
-
-	return block, nil
-}
-
-func loadPrivateKey() (crypto.PrivateKey, error) {
-	block, err := loadPemFile("./idp_key.pem")
-	if err != nil {
-		return nil, err
-	}
-
-	return x509.ParsePKCS8PrivateKey(block.Bytes)
-}
-
-func loadCertificate() (*x509.Certificate, error) {
-	block, err := loadPemFile("./idp_cert.pem")
-	if err != nil {
-		return nil, err
-	}
-
-	return x509.ParseCertificate(block.Bytes)
 }
